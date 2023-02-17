d0236551a5c32bb6389954dccad597fa880bec82
Sabba Petri <sabbap@gmail.com>
2023-02-17 10:18:53 +0400

    reordering home list

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { styles, useDash } from "../../../dash.config";
 import Icon from "react-native-vector-icons/Ionicons";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import {
   Dimensions,
   Pressable,
@@ -17,100 +17,109 @@ import { StackParamList } from "../routers";
 import { useNavigation } from "@react-navigation/native";
 import { StoryFilters } from "../../types/hn-api";
 import { ListItem } from "@rneui/themed";
+import DraggableFlatList, {
+  NestableDraggableFlatList,
+  NestableScrollContainer,
+  ScaleDecorator,
+} from "react-native-draggable-flatlist";
 import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content";
-
-type ListItemType = {
-  id: string;
-  header: string;
-  subheader: string;
-  iconName: string;
-  filter: StoryFilters;
-}[];
+import { listItems, ListItemType } from "./HomeList";
+import { usePreferences } from "../Settings/usePreferences";
 
 export const Home: FC<ListItemType> = () => {
   useDash();
   const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
+  const [homeOrderList, setHomeOrderList] = usePreferences(
+    "homeOrderList",
+    listItems
+  );
+  const [homeItems, setHomeItems] = useState(
+    homeOrderList ? homeOrderList[0] : listItems
+  );
   const {
     tokens: { color },
   } = useDash();
-  const listItems: ListItemType = [
-    {
-      id: "1",
-      header: "All HN",
-      subheader: "All HNs combined",
-      iconName: "ios-logo-hackernews",
-      filter: "top",
-    },
-    {
-      id: "2",
-      header: "Best HN",
-      subheader: "Best recent stories",
-      iconName: "star-half-outline",
-      filter: "best",
-    },
-    {
-      id: "3",
-      header: "New HN",
-      subheader: "Newest posts",
-      iconName: "ios-time-outline",
-      filter: "best",
-    },
-    {
-      id: "4",
-      header: "Show HN",
-      subheader: "Handmade projects",
-      iconName: "rocket-outline",
-      filter: "show",
-    },
-    {
-      id: "5",
-      header: "Ask HN",
-      subheader: "Questions and answers",
-      iconName: "bulb-outline",
-      filter: "ask",
-    },
-    {
-      id: "6",
-      header: "Jobs HN",
-      subheader: "Who's hiring?",
-      iconName: "file-tray-outline",
-      filter: "job",
-    },
-  ];
+
+  const persistOrder = (itemList) => {
+    setHomeItems(itemList);
+    setHomeOrderList(itemList);
+  };
+
+  useEffect(() => {
+    if (!!homeOrderList) {
+      setHomeItems(homeOrderList);
+    }
+  }, [homeOrderList]);
 
   return (
     <SafeAreaView style={containerBg()}>
       <View style={container()}>
-        <SectionList
-          ListHeaderComponent={<LogoHeader title="Select" />}
-          sections={[{ title: "Topics", data: listItems }]}
-          renderItem={({ item }) => (
-            <Pressable
-              onPress={() =>
-                navigation.navigate("Stories", {
-                  filter: item?.filter as StoryFilters,
-                })
-              }
-            >
-              <ListItem bottomDivider containerStyle={content()}>
-                <Icon
-                  name={item.iconName}
-                  color={color.textPrimary}
-                  size={25}
-                  style={image}
-                />
-                <ListItemContent>
-                  <ListItem.Title style={header()}>
-                    {item.header}
-                  </ListItem.Title>
-                  <ListItem.Subtitle style={subheader()}>
-                    {item.subheader}
-                  </ListItem.Subtitle>
-                </ListItemContent>
-              </ListItem>
-            </Pressable>
+        <LogoHeader title="Select" />
+        <DraggableFlatList
+          data={homeItems}
+          keyExtractor={(item: ListItemType) => item.id}
+          onDragEnd={({ data }) => persistOrder(data)}
+          renderItem={({ item, drag }) => (
+            <ScaleDecorator>
+              <Pressable
+                onLongPress={drag}
+                onPress={() =>
+                  navigation.navigate("Stories", {
+                    filter: item?.filter as StoryFilters,
+                  })
+                }
+              >
+                <ListItem bottomDivider containerStyle={content()}>
+                  <Icon
+                    name={item.iconName}
+                    color={color.textPrimary}
+                    size={25}
+                    style={image}
+                  />
+                  <ListItemContent>
+                    <ListItem.Title style={header()}>
+                      {item.header}
+                    </ListItem.Title>
+                    <ListItem.Subtitle style={subheader()}>
+                      {item.subheader}
+                    </ListItem.Subtitle>
+                  </ListItemContent>
+                </ListItem>
+              </Pressable>
+            </ScaleDecorator>
           )}
-        />
+        >
+          {/* <SectionList
+            ListHeaderComponent={<LogoHeader title="Select" />}
+            sections={[{ title: "Topics", data: listItems }]}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() =>
+                  navigation.navigate("Stories", {
+                    filter: item?.filter as StoryFilters,
+                  })
+                }
+              >
+                <ListItem bottomDivider containerStyle={content()}>
+                  <Icon
+                    name={item.iconName}
+                    color={color.textPrimary}
+                    size={25}
+                    style={image}
+                  />
+                  <ListItemContent>
+                    <ListItem.Title style={header()}>
+                      {item.header}
+                    </ListItem.Title>
+                    <ListItem.Subtitle style={subheader()}>
+                      {item.subheader}
+                    </ListItem.Subtitle>
+                  </ListItemContent>
+                </ListItem>
+              </Pressable>
+            )}
+          /> */}
+        </DraggableFlatList>
       </View>
     </SafeAreaView>
   );
