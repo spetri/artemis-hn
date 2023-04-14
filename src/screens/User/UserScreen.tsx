9649e043a067ba7e5e395031856be2f9fe7b0523
Sabba Petri <sabbap@gmail.com>
2023-04-14 23:19:47 +0400

    removing auth

--- src/screens/User/UserScreen.tsx
+++ src/screens/User/UserScreen.tsx
@@ -1,5 +1,4 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import cheerio from 'react-native-cheerio';
 import useSWR from 'swr';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { RefreshControl, SafeAreaView, Share, type ViewStyle } from 'react-native';
@@ -8,13 +7,9 @@ import { StoryCard } from '../../components/StoryCard/StoryCard';
 import { styles, useDash } from '../../../dash.config';
 import { type HackerNewsUser } from '../../types/hn-api';
 import { type StackParamList } from '../routers';
-import { HACKER_NEWS_API, HN, HN_LOGIN } from '../../constants/api';
+import { HACKER_NEWS_API } from '../../constants/api';
 import { keyExtractor } from '../../utils/util';
 import { FlashList } from '@shopify/flash-list';
-import { Button, Text } from '@rneui/themed';
-import parse from 'node-html-parser';
-import { usePreferencesStore } from '../../contexts/store';
-import { shallow } from 'zustand/shallow';
 
 export type UserScreenProps = NativeStackScreenProps<StackParamList, 'User'>;
 
@@ -31,20 +26,12 @@ export function UserScreen(props: UserScreenProps) {
       }).then(async (res) => await res.json())
   );
   const [didMount, setDidMount] = useState(false);
-  const { isLoggedIn, setIsLoggedIn } = usePreferencesStore(
-    (state) => ({
-      isLoggedIn: state.isLoggedIn,
-      setIsLoggedIn: state.setIsLoggedIn
-    }),
-    shallow
-  );
 
   useEffect(() => {
     if (user.data != null) {
       setDidMount(true);
     }
-    console.log('isLoggedIn:', isLoggedIn);
-  }, [user.data, isLoggedIn]);
+  }, [user.data]);
 
   const listHeaderComponent = useCallback(() => {
     return (
@@ -75,96 +62,6 @@ export function UserScreen(props: UserScreenProps) {
     );
   }, [id, props.navigation]);
 
-  // const getUser = (username) =>
-  //   fetch(`${HN}/user/${username}.json`)
-  //     .then((response) => response.json())
-  //     .then((responseJson) => responseJson)
-  //     .catch((error) => null);
-
-  // const login = (username, password) => {
-  //   const headers = new Headers({
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'Access-Control-Allow-Origin': '*'
-  //   });
-
-  //   return fetch(HN_LOGIN, {
-  //     method: 'POST',
-  //     headers,
-  //     body: `acct=${username}&pw=${password}&goto=news`,
-  //     mode: 'no-cors',
-  //     credentials: 'include'
-  //   })
-  //     .then((response) => {
-  //       return response.text();
-  //     })
-  //     .then((responseText) => {
-  //       const document = parse(responseText).querySelector('#logout');
-  //       if (document) {
-  //         setIsLoggedIn(true);
-  //       } else {
-  //         setIsLoggedIn(false);
-  //       }
-  //       if (!/Bad Login/i.test(responseText)) {
-  //         setIsLoggedIn(false);
-  //       } else {
-  //         setIsLoggedIn(false);
-  //       }
-  //     });
-  // const login = (username, password) => {
-  //   const headers = new Headers({
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'Access-Control-Allow-Origin': '*'
-  //   });
-  //   console.log('login', `acct=${username}&pw=${password}&goto=news`);
-
-  //   return fetch(`${HN}/login`, {
-  //     method: 'POST',
-  //     headers,
-  //     body: `acct=${username}&pw=${password}&goto=news`,
-  //     mode: 'no-cors',
-  //     credentials: 'include'
-  //   })
-  //     .then((response) => {
-  //       return response.text();
-  //     })
-  //     .then((responseText) => {
-  //       console.log('awdwad', responseText);
-  //       console.log(!/Bad Login/i.test(responseText));
-  //       return !/Bad Login/i.test(responseText);
-  //     });
-  // };
-
-  // const getLogoutUrl = () =>
-  //   fetch(`${HN}/news`, {
-  //     mode: 'no-cors',
-  //     credentials: 'include'
-  //   })
-  //     .then((response) => response.text())
-  //     .then((responseText) => {
-  //       const document = cheerio.load(responseText);
-  //       const test = document('#logout').attr('href');
-  //       console.log(test);
-  //     });
-
-  // const logout = () =>
-  //   getLogoutUrl()
-  //     .then((logoutUrl) => {
-  //       console.log('url', `${HN}/${logoutUrl}`);
-  //       return fetch(`${HN}/${logoutUrl}`, {
-  //         mode: 'no-cors',
-  //         credentials: 'include'
-  //       });
-  //     })
-  //     .then((response) => response.text())
-  //     .then((responseText) => {
-  //       setIsLoggedIn(false);
-  //       return true;
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //       return false;
-  //     });
-
   const refreshControl = useMemo(
     () => (
       <RefreshControl
@@ -177,12 +74,6 @@ export function UserScreen(props: UserScreenProps) {
 
   return (
     <SafeAreaView style={container()}>
-      {/* <Button type="outline" onPress={() => login('pookieinc', 'shasta99')}>
-        <Text>Login</Text>
-      </Button>
-      <Button type="outline" onPress={() => logout()}>
-        <Text>Logout</Text>
-      </Button> */}
       <FlashList
         ListHeaderComponent={listHeaderComponent}
         stickyHeaderIndices={[0]}
